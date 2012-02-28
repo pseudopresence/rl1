d732fd5a8cf9919c8234bef03e0ae3f630d9a6d2
Chris Swetenham <cswetenham@gmail.com>
2012-02-28 15:05:56 +0000

    Add some notes on design.

--- main.m
+++ main.m
@@ -1,8 +1,26 @@
+%% Design
+
+% Firstly, since we are working in Matlab, we will not attempt to abstract
+% over the details of the problem and produce a general algorithm, since
+% the facilities available for doing so are relatively limited. Instead we
+% will specify the problem and its representation and then write algorithms
+% that solve the specific problem. That said, we will abstract a little
+% where it is useful for code reuse between similar code for our
+% algorithms.
+
+% In some places we will make use of the fact that we have a tabular
+% representation of a function, to speed up the algorithm; in others we
+% will allow for a function to be either represented as a Matlab table or
+% as a Matlab function, which will be either called or indexed with the
+% same syntax, e.g. Q(S, A).
+
+%% Setup
+
 clc;
 clear all;
 close all;
 
-%% Setup
+%% Part I - gridworld, dynamic programming
 
 Rot90 = [
      0 +1;
@@ -29,9 +47,8 @@ posFromState = @(S) [mod((S - 1),MapWidth) + 1, floor((S - 1)/MapWidth) + 1];
 
 
 %% Walls
-% TODO 'vertical' and 'horizontal' are the wrong way around
-% Vertical wall representation: Y, StartX, EndX
-Walls_V = [
+% Horizontal wall representation: Y, StartX, EndX
+Walls_H = [
     0.5, 0.5, 8.5;
     1.5, 0.5, 1.5;
     1.5, 7.5, 8.5;
@@ -40,8 +57,8 @@ Walls_V = [
     8.5, 0.5, 8.5
 ];
 
-% Horizontal wall representation: X, StartY, EndY
-Walls_H = [
+% Vertical wall representation: X, StartY, EndY
+Walls_V = [
     0.5, 0.5, 8.5;
     2.5, 4.5, 6.5;
     3.5, 3.5, 5.5;
@@ -53,20 +70,6 @@ Walls_H = [
 
 % Compute state transition matrix from walls
 
-Arrow(1, :, :) = [
-    0.25 0;
-    0.75 0;
-    0.625 -0.125;
-    0.75 0;
-    0.625 +0.125;
-    0.75 0;
-];
-% Rotate for other directions
-for A = 2:4
-    for L = 1:size(Arrow, 2)
-        Arrow(A,L,:) = Rot90^(A-1) * squeeze(Arrow(1,L,:));
-    end
-end
 % [State x Action] -> State
 StateTransitions = zeros([NStates, NActions]);
 
@@ -79,15 +82,15 @@ for A = 1:NActions
         NP = Actions(A,:) + P;
         MoveOK = 1;
         
-        for W = 1:size(Walls_V, 1)
-            WS = [Walls_V(W, 2) Walls_V(W, 1)];
-            WE = [Walls_V(W, 3) Walls_V(W, 1)];
+        for W = 1:size(Walls_H, 1)
+            WS = [Walls_H(W, 2) Walls_H(W, 1)];
+            WE = [Walls_H(W, 3) Walls_H(W, 1)];
             MoveOK = MoveOK & ~testSegmentSegment(P, NP, WS, WE);
         end
         
-        for W = 1:size(Walls_H, 1)
-            WS = [Walls_H(W, 1) Walls_H(W, 2)];
-            WE = [Walls_H(W, 1) Walls_H(W, 3)];
+        for W = 1:size(Walls_V, 1)
+            WS = [Walls_V(W, 1) Walls_V(W, 2)];
+            WE = [Walls_V(W, 1) Walls_V(W, 3)];
             MoveOK = MoveOK & ~testSegmentSegment(P, NP, WS, WE);
         end
         
@@ -114,7 +117,23 @@ StartPolicy = [
     1 2 3 3 1 1 2 4
 ]';
 
-% Glyphs for rendering policies
+% Arrows for rendering state transitions
+Arrow(1, :, :) = [
+    0.25 0;
+    0.75 0;
+    0.625 -0.125;
+    0.75 0;
+    0.625 +0.125;
+    0.75 0;
+];
+% Rotate for other directions
+for A = 2:4
+    for L = 1:size(Arrow, 2)
+        Arrow(A,L,:) = Rot90^(A-1) * squeeze(Arrow(1,L,:));
+    end
+end
+
+% Arrows for rendering policies
 % TODO make X/Y component the last, the remove the squeeze()
 % TODO remove the transpose
 % TODO generate with rotation matrix
@@ -136,7 +155,7 @@ ActionGlyphs(2, 5, :, :) = [-1 +1; -1 +1]';
 % Rescale
 ActionGlyphs = 0.25 * ActionGlyphs;
 
-drawWalls = @() drawWalls(Walls_V, Walls_H);
+drawWalls = @() drawWalls(Walls_H, Walls_V);
 drawStateTransitions = @(StateTransitions) drawStateTransitions(Arrow, StateTransitions, posFromState);
 drawPolicy = @(Policy) drawPolicy(ActionGlyphs, Policy, posFromState);
 
@@ -146,19 +165,10 @@ figure;
     drawPolicy(StartPolicy);
 axis([0.5, 8.5, 0.5, 8.5], 'xy', 'square');
 
-
-
-
-%% visualisation for grid world with walls
-%% visualisation for policies, showing arrow for direction
-
-%% TODO - visualisation for state-value function
 %% TODO - implement state transition probs PP(a, s, s') as a function that
 %% PP(s, a) -> [States, Probs]; a list of non-zero next states and their
 %% probabilities? Or PP(s, a, rnd) -> new state at random according to
 %% distribution
-
-%% Part I - gridworld, dynamic programming
 
 reward = @(S, A, S2) -1 * (S ~= GoalState);
 
@@ -178,8 +188,7 @@ for PP = 1:MaxPolicyIterations
     Policy = NewPolicy;
     
     figure(2);
-        V2D = reshape(V, [MapWidth MapHeight]);
-        imagesc(V2D);
+        imagesc(reshape(V, MapSize));
         colormap('gray');
         drawWalls();
         drawPolicy(Policy);
@@ -211,8 +220,7 @@ for PP = 1:MaxPolicyIterations
     Policy = improvePolicy(V, StateTransitions, reward, Discount);
     
     figure(3);
-        V2D = reshape(V, [MapWidth MapHeight]);
-        imagesc(V2D);
+        imagesc(reshape(V, MapSize));
         colormap('gray');
         drawWalls();
         drawPolicy(Policy);
