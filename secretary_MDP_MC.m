cca10fcc5fcb9166719ad9f5fb4c701408c18bad
Chris Swetenham <cswetenham@gmail.com>
2012-02-29 16:04:37 +0000

    We only need to optimise Q, Policy when we actually visit a state. LUDICROUS SPEED

--- secretary_MDP_MC.m
+++ secretary_MDP_MC.m
@@ -33,7 +33,7 @@ TotalReturn = zeros([NCandidates, NCandidates, 2]);
 VisitCount = zeros([NCandidates, NCandidates, 2]);
 
 tic;
-MaxEpisodes = 2500000;
+MaxEpisodes = 25000000;
 for Episode = 1:MaxEpisodes
     % Each episode, we will generate 30 random candidate values
     % Without loss of generality we will interview them in order 1-N
@@ -67,23 +67,6 @@ for Episode = 1:MaxEpisodes
         end
         SortedSoFar(R) = C(K);
         
-        %disp(SortedSoFar);
-        %disp([K R]);
-        %pause(1);
-%         CS = C(1:K);
-%         [Dummy I] = sort(CS, 'descend');
-%         Idx = 1:K;
-%         Idx = Idx(I);
-%         I2 = [I' zeros([1, NCandidates - K])];
-%         R2 = Idx(K);
-%         if (R ~= R2)
-%             disp(SortedSoFar);
-%             disp([K R]);
-%             pause(1);
-%         end
-        %disp([C'; I2]);
-        %pause(1);
-        
         if (K == NCandidates)
             Action = 2;
         elseif (rand(1) < Epsilon)
@@ -99,43 +82,53 @@ for Episode = 1:MaxEpisodes
     MaxK = K;
     Reward = C(K);
     for K = 1:MaxK
-        % disp([K Rs(K)]);
-        % pause(1);
         A = (K == MaxK) + 1;
-        VisitCount(K, Rs(K), A) = VisitCount(K, Rs(K), A) + 1;
-        TotalReturn(K, Rs(K), A) = TotalReturn(K, Rs(K), A) + Reward;
+        R = Rs(K);
+                
+        NV = VisitCount(K, R, A) + 1;
+        VisitCount(K, R, A) = NV;
+        NT = TotalReturn(K, R, A) + Reward;
+        TotalReturn(K, R, A) = NT;
+        Q(K, R, A) = NT / NV;
+        
+        Policy(K, R) = (Q(K, R, 1) < Q(K, R, 2)) + 1;
     end
     
-    VC = max(VisitCount, 1);
-    Q = TotalReturn ./ VC;
-    [Dummy, Policy] = max(Q, [], 3);
-    
-    %if max(max(max(abs(Q - OldQ),[],1),[],2),[],3) < 0.00001
-    %    break;
-    %end
-    
-%    disp(Episode);
+    % VC = max(VisitCount, 1);
+    % Q = TotalReturn ./ VC;
+    % [Dummy, Policy] = max(Q, [], 3);
 end
 for K = 1:NCandidates
     for R = (K+1):NCandidates
         Policy(K, R) = 0;
     end
 end
-fprintf('Episodes to convergence: %d\n', Episode);
 toc;
 figure;
 imagesc(Policy');
 axis xy;
+axis square;
 xlabel('Step');
 ylabel('Rank');
+colormap('gray');
+% writeFigurePDF('SecretaryMDPPolicy.pdf');
 figure;
 imagesc(Q(:,:,1)');
 axis xy;
+axis square;
 xlabel('Step');
 ylabel('Rank');
+colormap('gray');
+% writeFigurePDF('SecretaryMDPQFunction.pdf');
 figure;
-imagesc(Q(:,:,2)');
+[VG, Dummy] = max(Q, [], 3);
+VE = 0.5 * sum(Q, 3);
+V = (1-Epsilon) * VG + Epsilon * VE;
+imagesc(V');
 axis xy;
+axis square;
 xlabel('Step');
 ylabel('Rank');
+colormap('gray');
+% writeFigurePDF('SecretaryMDPVFunction.pdf');
 end
